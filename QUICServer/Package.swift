d1011bc9dfb79d4884b1687466e69d679913113a
Yuval Fradkin <yuval@research.local>
2026-09-08 08:06:46 +0300

    fix: pin swift-crypto to satisfy 0.2.2 deps

--- QUICServer/Package.swift
+++ QUICServer/Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio-quic.git", exact: "0.2.2"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "5.0.0-beta.2"..<"6.0.0"),
     ],
     targets: [
         .executableTarget(
